e61cbe0f975b7355d2fe0033eca7788f93b82ac8
temurhamidov <temur20012007@gmail.com>
2022-08-19 14:46:13 +0500

    laravel translate packgist

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+
+</head>
+    <body>
+    <ul>
+        <li><a href="{{ url('/') }}">EN</a></li>
+        <li><a href="{{ url('/ru') }}">RU</a></li>
+        <li><a href="{{ url('/uz') }}">UZ</a></li>
+    </ul>
+        <h2>{{__('List of staple foods')}}</h2>
+    <ol>
+        <li>{{__('Potatoes') }}</li>
+        <li>{{__('Onion') }}</li>
+        <li>{{__('Rice')}}</li>
+        <li>{{ __('Mather') }}</li>
+        <li>{{ __('Father') }}</li>
+    </ol>
+    </body>
+</html>
